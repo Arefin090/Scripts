149b82115d5cd94e36095c05c4eeec7aa83fc943
Arefin090 <101785779+Arefin090@users.noreply.github.com>
2023-10-20 05:23:13 +1100

    security update

--- Daily_joke_sender.py
+++ Daily_joke_sender.py
@@ -1,12 +1,22 @@
+import os
 import smtplib
 import requests
 from email.mime.text import MIMEText
 import keyring
 from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from the .env file
+
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+RECIPIENT_EMAILS = [email.strip() for email in os.getenv("RECIPIENT_EMAILS").split(',')]
+GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
+
 def fetch_joke():
     url = "https://dad-jokes.p.rapidapi.com/random/joke"
     headers = {
-        "X-RapidAPI-Key": "2d157e36ebmshb07e9adfee0dcb3p13048cjsnb3eb119c23de",
+        "X-RapidAPI-Key": RAPIDAPI_KEY,
         "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
     }
 
@@ -20,12 +30,11 @@ def fetch_joke():
     # Formatting the joke
     return f"{setup} ... {punchline}"
 
-def send_email(joke, recipient_email):
-    sender_email = "dailyjokesbyarefin@gmail.com"
+def send_email(joke, recipient_emails):
     
-    # Retrieve the stored Gmail App Password from macOS Keychain
-    sender_password = keyring.get_password("daily_joke_sender", "GMAIL_APP_PASSWORD")
-    
+    # Retrieve the stored Gmail App Password from environment variable or macOS Keychain
+    sender_password = GMAIL_APP_PASSWORD if GMAIL_APP_PASSWORD else keyring.get_password("daily_joke_sender", "GMAIL_APP_PASSWORD")
+
     subject = "Arefin's Daily Joke"
     
     html_template = """
@@ -34,7 +43,8 @@ def send_email(joke, recipient_email):
         <body>
             <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; border: 1px solid #e9e9e9; padding: 20px;">
                 <h2 style="color: #4a90e2;">Arefin's Daily Joke</h2>
-                <p>Hey there,</p>
+                <p>Hey mate,</p>
+                <p>This email serves as a token of appreciation I have for you. Here's your daily joke:</p>
                 <p style="font-size: 18px; font-weight: bold; padding: 10px; background-color: #f9f9f9; border: 1px solid #e9e9e9;">{{joke}}</p>
                 <p>Hope that brought a smile to your face! Stay tuned for more jokes tomorrow.</p>
                 <p>Best,</p>
@@ -46,20 +56,22 @@ def send_email(joke, recipient_email):
     
     html_content = html_template.replace("{{joke}}", joke)
 
-    msg = MIMEMultipart("alternative")
-    msg["From"] = sender_email
-    msg["To"] = recipient_email
-    msg["Subject"] = subject
-    
-    part = MIMEText(html_content, "html")
-    msg.attach(part)
-
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(sender_email, sender_password)
-    server.sendmail(sender_email, recipient_email, msg.as_string())
+    server.login(SENDER_EMAIL, sender_password)
+
+    for email in recipient_emails:
+        msg = MIMEMultipart("alternative")
+        msg["From"] = SENDER_EMAIL
+        msg["To"] = email
+        msg["Subject"] = subject
+
+        part = MIMEText(html_content, "html")
+        msg.attach(part)
+
+        server.sendmail(SENDER_EMAIL, email, msg.as_string())
+
     server.quit()
 
 if __name__ == "__main__":
     joke = fetch_joke()
-    recipient = "arefin923@gmail.com"
-    send_email(joke, recipient)
+    send_email(joke, RECIPIENT_EMAILS)
